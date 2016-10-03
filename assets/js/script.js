fd0955c311ff2c9e2773ed08b52418b0f9fb2488
Vinny <espinalv1@gmail.com>
2016-10-03 18:56:04 -0400

    Toggle bug fix.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,19 @@
 var navToggleList = document.getElementById('nav-toggle-list');
 var navToggle = document.getElementById('nav-toggle');
 var mainNav = document.getElementById('main-nav');
+var mainNavClone = mainNav.cloneNode(true);
 
 navToggle.addEventListener('click', function(e) {
   e.preventDefault();
 
   if (navToggleList.classList.contains('hidden')) {
-    navToggleList.appendChild(mainNav);
+    navToggleList.appendChild(mainNavClone);
   } else {
-    navToggleList.removeChild(mainNav);
+    navToggleList.removeChild(mainNavClone);
   }
 
   navToggleList.classList.toggle('hidden');
 });
+
+/* TODO
+Resolve toggle bug. When Menu is toggled, it removes mainNav from header */
