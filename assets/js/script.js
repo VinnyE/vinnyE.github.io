d34da9b00d8e0a871d30379ad2fcfed8cfa10fd1
Vinny <espinalv1@gmail.com>
2016-10-03 17:58:42 -0400

    Add breakpoints, skills icons, fixed menu toggling, page wrapper.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,15 @@
 var navToggleList = document.getElementById('nav-toggle-list');
 var navToggle = document.getElementById('nav-toggle');
+var mainNav = document.getElementById('main-nav');
 
-navToggle.addEventListener('click', function() {
+navToggle.addEventListener('click', function(e) {
+  e.preventDefault();
+
+  if (navToggleList.classList.contains('hidden')) {
+    navToggleList.appendChild(mainNav);
+  } else {
+    navToggleList.removeChild(mainNav);
+  }
+
   navToggleList.classList.toggle('hidden');
-  console.log('hello');
 });
